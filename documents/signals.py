47f5c1a450f3c3291fc58054786446b8f58d36f6
Aleksey <aleksey@Alekseys-iMac.local>
2025-08-11 00:27:24 +0300

    Созданы оповещения и задачи

---
+++ documents/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Document
+from .tasks import send_admin_new_document_email
+
+@receiver(post_save, sender=Document)
+def notify_admin_on_create(sender, instance: Document, created, **kwargs):
+    if created:
+        send_admin_new_document_email.delay(instance.pk)
