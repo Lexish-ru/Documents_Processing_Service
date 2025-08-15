858dbb877daee2c8584c193c369a486f14c07a0c
Aleksey <aleksey@Alekseys-iMac.local>
2025-08-15 20:15:16 +0300

    выполнена проверка линтером

--- documents/api/views.py
+++ documents/api/views.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets, permissions, parsers
 from documents.models import Document
 from .serializers import DocumentSerializer
 
+
 class IsOwnerOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner_id == request.user.id
+
 
 class DocumentViewSet(viewsets.ModelViewSet):
     serializer_class = DocumentSerializer
